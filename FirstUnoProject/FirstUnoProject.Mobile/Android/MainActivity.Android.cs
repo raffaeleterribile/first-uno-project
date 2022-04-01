756de07e478a42719ebf3cf27b6dde16c07f526c
Raffaele Terribile <raff-github@usa.net>
2022-04-01 17:47:16 +0200

    feat: Project creation

---
+++ FirstUnoProject/FirstUnoProject.Mobile/Android/MainActivity.Android.cs
@@ -0,0 +1,16 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace FirstUnoProject {
+	[Activity(
+			MainLauncher = true,
+			ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+	public class MainActivity : Windows.UI.Xaml.ApplicationActivity {
+	}
+}
+
---
+++ FirstUnoProject/FirstUnoProject.Skia.Wpf/Program.cs
@@ -0,0 +1,2 @@
+﻿namespace FirstUnoProject.Skia.Gtk {
+}
